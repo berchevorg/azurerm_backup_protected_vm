42cb6af30be5e633646c76a2c0337afae95bd7e8
Georgi Berchev a.k.a Georgiman <g.berchev@gmail.com>
2022-01-28 12:22:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ resource "azurerm_backup_policy_vm" "example" {
 }
 
 resource "azurerm_backup_protected_vm" "vm1" {
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.main.name
   recovery_vault_name = azurerm_recovery_services_vault.example.name
   source_vm_id        = azurerm_virtual_machine.main.id
   backup_policy_id    = azurerm_backup_policy_vm.example.id
